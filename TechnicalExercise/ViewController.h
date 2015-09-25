995d1c4669fbbf22fc085d066893bc9514441e14
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-09-25 01:04:04 -0700

    Initial Commit

---
+++ TechnicalExercise/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TechnicalExercise
+//
+//  Created by Ronald Hernandez on 9/25/15.
+//  Copyright © 2015 Hardcoder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
